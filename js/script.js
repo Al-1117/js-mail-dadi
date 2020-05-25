45f51f06d6c4ee62d087813516efb9e2cfd781ad
Alioune <alioune1117@gmail.com>
2020-05-25 16:17:52 +0200

    fatto primo esercizio

---
+++ js/script.js
@@ -0,0 +1,37 @@
+// Array contenente Elenco delle email degli invitati
+
+var indirizziEmail = ['marco@gmail.com', 'luca@libero.it', 'laura@hotmail.com', 'franca@outlook.it', 'severino@gmail.com', 'leonardo@gmail.com', 'pippo@hotmail.com', 'antonio@virgilio.it', 'angela@gmail.com'];
+
+// console.log(indirizziEmail[4]);
+
+
+// Messaggio Esito Invito
+bottoneInvito = document.getElementById('btn_invito');
+var esitoInvito = document.getElementById('esito');
+
+
+// Chiedo all'utente la sua email
+emailUtente = document.getElementById('indirizzo_email');
+
+// Controllo che l'email sia presente
+
+// Evento cliccando il tasto controllo invito
+
+bottoneInvito.addEventListener('click',
+function () {
+
+  for ( var i = 0; i < indirizziEmail.length; i++ ) {
+    // console.log(indirizziEmail[i]);
+    if (indirizziEmail[i] == emailUtente.value) {
+      esitoInvito.innerHTML = "Fai parte degli invitati alla festa, benvenuto! :-)";
+    }
+    else {
+      esitoInvito.innerHTML = "Mi dispiace, ma non fai parte degli invitati alla festa. Sarà per l'anno prossimo :-(";
+    }
+
+
+  }
+
+}
+
+);
